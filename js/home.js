323012a84403d1b679cae939a3c9e6c3fda04b68
Nazar Buk <nazarbuk27@gmail.com>
2024-12-17 13:31:33 +0200

    Add Header section
    🧱 Add Add Header section
    🔧 Add hover effect to CAT buttons.
    📸 Add images.
    📱 Add responsive for header mobile menu.
    🎨 Add styles for Header .
    🧑‍💻 Add js logic for mobile menu.

--- js/home.js
+++ js/home.js
@@ -1,1 +1,22 @@
-// console.log("lalala");
+const menuBtn = document.querySelector(".menu__icon");
+const menu = document.querySelector(".nav");
+const body = document.body;
+
+// console.log(menuBtn, "menuBtn");
+console.log(menu, "menumenuBtn");
+
+if (menu && menuBtn) {
+  menuBtn.addEventListener("click", () => {
+    menuBtn.classList.toggle("active");
+    menu.classList.toggle("active");
+    body.classList.toggle("lock");
+  });
+}
+
+menu.addEventListener("click", (e) => {
+  if (e.target.classList.contains("nav")) {
+    menuBtn.classList.remove("active");
+    menu.classList.remove("active");
+    body.classList.remove("lock");
+  }
+});
